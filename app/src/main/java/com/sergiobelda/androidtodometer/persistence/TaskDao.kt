4b61bc15457ab7f66ebe4cb3d7d7796aeaf7de64
Sergio Belda Galbis <serbelga@inf.upv.es>
2020-04-10 00:53:01 +0200

    Update build.gradle to 4.1.0 canary 5
    Remove rest project to avoid IDE problems

--- app/src/main/java/com/sergiobelda/androidtodometer/persistence/TaskDao.kt
+++ app/src/main/java/com/sergiobelda/androidtodometer/persistence/TaskDao.kt
@@ -22,26 +22,26 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTask(task: Task)
+
     @Query("SELECT * FROM task_table ORDER BY taskId ASC")
     fun getTasks(): Flow<List<Task>>
 
     @Query("SELECT * FROM task_table WHERE taskId = :id")
     fun getTask(id: Int): Flow<Task>
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertTask(task: Task)
-
-    @Query("DELETE FROM task_table")
-    suspend fun deleteTasks()
+    @Query("DELETE FROM task_table WHERE taskId = :id")
+    suspend fun deleteTask(id: Int)
 
     @Delete
     suspend fun deleteTask(task: Task)
 
+    @Query("DELETE FROM task_table")
+    suspend fun deleteTasks()
+
     @Update
     suspend fun updateTask(task: Task)
-
-    @Query("DELETE FROM task_table WHERE taskId = :id")
-    suspend fun deleteTask(id: Int)
 
     @Query("UPDATE task_table SET taskState = 1 WHERE taskId = :id")
     suspend fun setTaskDone(id: Int)
