376d093dca1b82263989bed4873b2865b551e829
Sergio Belda Galbis <serbelga@inf.upv.es>
2021-01-23 01:23:03 +0100

    Enabled code shrinking, obfuscation, and shrink resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,8 @@ android {
     }
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isShrinkResources = true
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
