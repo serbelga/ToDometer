cdb8bb31fe658faea14a4b27d3b6ebdb2a492b30
Sergio Belda Galbis <serbelga@inf.upv.es>
2021-03-13 19:56:01 +0100

    Upgrade version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sergiobelda.androidtodometer"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 8
-        versionName = "1.1.0-beta04"
+        versionCode = 9
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
