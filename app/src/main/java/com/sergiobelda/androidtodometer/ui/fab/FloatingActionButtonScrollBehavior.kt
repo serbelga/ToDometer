de33d7987d208297713e44ee4f47dcc8e5fe4851
Sergio Belda Galbis <serbelga@inf.upv.es>
2021-03-13 01:28:36 +0100

    New FloatingActionButtonScrollBehavior.

---
+++ app/src/main/java/com/sergiobelda/androidtodometer/ui/fab/FloatingActionButtonScrollBehavior.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 Sergio Belda Galbis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sergiobelda.androidtodometer.ui.fab
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class FloatingActionButtonScrollBehavior(
+    context: Context,
+    attrs: AttributeSet? = null
+) : FloatingActionButton.Behavior(context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        directTargetChild: View,
+        target: View,
+        nestedScrollAxes: Int,
+        type: Int
+    ): Boolean {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
+            child.hide(object : FloatingActionButton.OnVisibilityChangedListener() {
+
+                override fun onHidden(fab: FloatingActionButton?) {
+                    super.onHidden(fab)
+                    fab?.visibility = View.INVISIBLE
+                }
+            })
+        } else if (dyConsumed <= 0 && child.visibility != View.VISIBLE) {
+            child.show()
+        }
+    }
+}
