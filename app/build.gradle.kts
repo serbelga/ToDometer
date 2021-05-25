7789922bc61534eee6ef9fdacad55cd2cbb22996
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2021-05-25 22:42:41 +0200

    Update versionCode and versionName to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sergiobelda.androidtodometer"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 9
-        versionName = "1.1.0"
+        versionCode = 10
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
