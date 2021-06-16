fb80241fd2ca60fcdbaeadb1e77c126f8aac242d
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2021-06-16 23:52:17 +0200

    Update versionCode and versionName to 1.2.0-alpha03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sergiobelda.androidtodometer"
         minSdkVersion(24)
         targetSdkVersion(30)
-        versionCode = 12
-        versionName = "1.2.0-alpha02"
+        versionCode = 13
+        versionName = "1.2.0-alpha03"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
