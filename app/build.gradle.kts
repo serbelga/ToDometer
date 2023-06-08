95fbca735b3f965425104cc7f5b2d36e4d29c47a
Sergio Belda <sbeldagalbis@gmail.com>
2023-06-08 13:29:17 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        jvmToolchain(17)
     }
     buildTypes {
         getByName("release") {
